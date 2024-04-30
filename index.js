6411143bc4d046cf7383e6e453c26077565596d5
Luis Perez <lperezdev843@gmail.com>
2024-04-29 23:59:30 -0400

    getters

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ console.log("hello world");
 
 function Circle(radius) {
   this.radius = radius;
+
+  let defaultLocation = { x: 0, y: 0 };
+  this.getDefaultLocation = function () {
+    return defaultLocation;
+  };
   this.draw = function () {
     console.log("draw");
   };
+
+  Object.defineProperties(this, "defaultLocation", {
+    get: function ()
+    {
+      return defaultLocation;
+  } });
 }
 
 const circle = new Circle(10);
+circle.getDefaultLocation;
 
-for (let key in circle)
-{
-    if (typeof circle[key] !== "function")
-  console.log(key, circle[key]);
+for (let key in circle) {
+  if (typeof circle[key] !== "Number") console.log(key, circle[key]);
 }
 
 const keys = Object.keys(circle);
-console.log(keys) 
+console.log(keys);
+
+if ("radius" in circle) console.log("Circle has a radius.");
